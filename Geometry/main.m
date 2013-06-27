246c8c45c5f98d1eeb3fb8b21f8d5372e5f8bcbb
jeffreyvarner <jeff@curveos.com>
2013-06-27 18:14:01 -0400

    Initial Commit

---
+++ Geometry/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Geometry
+//
+//  Created by Jeffrey Varner on 6/27/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
